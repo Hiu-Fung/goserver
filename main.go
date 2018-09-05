6596b16d4d4d708ade00aa31ce53ef8278d32267
Hiu-Fung Hau <dafungz@htomail.com>
2018-09-05 22:48:03 +0200

    Initial commit w/ working websocket print

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Name string      `json:"name"`
+	Date interface{} `json:"data"`
+}
+
+type Channel struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func main() {
+	// fmt.Println("Hello from go")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":4001", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Hello from goserver")
+	socket, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		// msgType, msg, err := socket.ReadMessage()
+
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
+		var inMessage Message
+		if err := socket.ReadJSON(&inMessage); err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		fmt.Println("%#v\n", inMessage) //https://jsbin.com/rixicuzivu/edit?js,console,output
+		// fmt.Println(string(msg))
+
+		// if err = socket.WriteMessage(msgType, msg); err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
+	}
+}
