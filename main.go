e15a6166b6962f3cf3395cb672fabac716ca6a52
Hiu-Fung Hau <dafungz@htomail.com>
2018-09-29 16:42:53 +0200

    Add subscribe- & addChannel handlers

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
 )
 
 type Message struct {
 	Name string      `json:"name"`
-	Date interface{} `json:"data"`
+	Data interface{} `json:"data"`
 }
 
 type Channel struct {
@@ -39,18 +41,59 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for {
-		msgType, msg, err := socket.ReadMessage()
+		// msgType, msg, err := socket.ReadMessage()
 
-		if err != nil {
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
+		var inMessage Message
+		var outMessage Message
+		if err := socket.ReadJSON(&inMessage); err != nil {
 			fmt.Println(err)
-			return
+			break
 		}
 
-		fmt.Println(string(msg))
+		fmt.Printf("%#v\n", inMessage) //https://jsbin.com/rixicuzivu/edit?js,console,output
 
-		if err = socket.WriteMessage(msgType, msg); err != nil {
-			fmt.Println(err)
-			return
+		switch inMessage.Name {
+		case "channel add":
+			err := addChannel(inMessage.Data)
+			if err != nil {
+				outMessage = Message{"error", err}
+				if err := socket.WriteJSON(outMessage); err != nil {
+					fmt.Println(err)
+					break
+				}
+			}
+		case "channel subscribe":
+			err := subscribeChannel()
 		}
+		// fmt.Println(string(msg))
+
+		// if err = socket.WriteMessage(msgType, msg); err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
+	}
+}
+
+func addChannel(data interface{}) error {
+	var channel Channel
+
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		return err
+	}
+
+	channel.Id = "1"
+	fmt.Printf("%#v\n", channel)
+	return nil
+}
+
+func subscribeChannel() {
+	//TODO: rethinkDB Query / changefeed
+	for {
+		time.Sleep(time.Second * 1)
 	}
 }
